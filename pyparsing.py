65db4bcafb3b623a0a6b72206e240357877f9183
Cengiz Kaygusuz <cngkaygusuz@gmail.com>
2019-08-14 12:34:35 -0400

    Fix punctuation for `ParseBaseException._from_exception`.

--- pyparsing.py
+++ pyparsing.py
@@ -293,8 +293,8 @@ class ParseBaseException(Exception):
     @classmethod
     def _from_exception(cls, pe):
         """
-        internal factory method to simplify creating one type of ParseException
-        from another - avoids having __init__ signature conflicts among subclasses
+        Internal factory method to simplify creating one type of ParseException
+        from another: avoids having __init__ signature conflicts among subclasses.
         """
         return cls(pe.pstr, pe.loc, pe.msg, pe.parserElement)
 
